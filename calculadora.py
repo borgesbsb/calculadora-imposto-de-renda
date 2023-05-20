4366d5ac8f73054dd529b199713e26702d57e0d2
borgesbsb <borges.bnjamin@gmail.com>
2023-05-20 18:49:08 -0400

    feat: Added new files or features in  M  calculadora.py A  "informa\303\247\303\265es_consolidadas.csv" M  main2.py

--- calculadora.py
+++ calculadora.py
@@ -1,26 +1,23 @@
 import pandas as pd
 
+
 class Calculadora:
     def __init__(self, csv_file):
-        self.df = pd.read_csv(csv_file,parse_dates=['Data da operação'])
-        self.carteira = pd.DataFrame(columns=['Data', 'Nome','Preço', 'Preco Medio','Quantidade','Quantidade Media', 'Oper', 'Resultado Auferido', 'Lucro/Prejuizo'])
+        self.df = pd.read_csv(csv_file, parse_dates=['Data da operação'])
+        self.carteira = pd.DataFrame(columns=['Data', 'Nome', 'Preço', 'Preco Medio',
+                                     'Quantidade', 'Quantidade Media', 'Oper', 'Valor Executado', 'Resultado Auferido', 'Lucro/Prejuizo'])
         self.lucro_prejuizo = 0
-        
 
-
-
-    
     def calcula_imposto(self, resultado_auferido_mes):
         if resultado_auferido_mes > 0:
             return resultado_auferido_mes * 0.15
         return 0
-    
+
     def total_compra():
         return self.carteira.loc[self.carteira['Oper'] == 'Compra', 'Quantidade'].sum()
-    
+
     def total_venda():
         return self.carteira.loc[self.carteira['Oper'] == 'Venda', 'Quantidade'].sum()
-        
 
     def calcular_carteira(self):
         preco_medio = 0
@@ -35,21 +32,30 @@ class Calculadora:
             taxa_corretagem = row['Taxa de corretagem']
 
             if self.carteira.loc[self.carteira['Nome'] == acao].empty:
-                preco_medio = ((preco * quantidade) + taxa_corretagem) / quantidade
-                self.carteira.loc[index] = [data_operacao, acao, preco, preco_medio,quantidade, quantidade, operacao, 0, self.lucro_prejuizo]
+                preco_medio = ((preco * quantidade) +
+                               taxa_corretagem) / quantidade
+                self.carteira.loc[index] = [data_operacao, acao, preco, preco_medio,
+                                            quantidade, quantidade, operacao,preco_medio*quantidade , 0, self.lucro_prejuizo]
             else:
-                preco_medio_tmp = self.carteira.loc[self.carteira['Nome'] == acao, 'Preco Medio'].values[-1]
-                quantidade_media_tmp = self.carteira.loc[self.carteira['Nome'] == acao, 'Quantidade Media'].values[-1]
-                resultado_auferido_temp = self.carteira.loc[self.carteira['Nome'] == acao, 'Resultado Auferido'].values[-1]
+                preco_medio_tmp = self.carteira.loc[self.carteira['Nome']
+                                                    == acao, 'Preco Medio'].values[-1]
+                quantidade_media_tmp = self.carteira.loc[self.carteira['Nome']
+                                                         == acao, 'Quantidade Media'].values[-1]
+                resultado_auferido_temp = self.carteira.loc[self.carteira['Nome']
+                                                            == acao, 'Resultado Auferido'].values[-1]
 
                 if operacao == 'Compra':
-                    preco_medio = ((preco_medio_tmp * quantidade_media_tmp) + (preco * quantidade) + taxa_corretagem) / (quantidade_media_tmp + quantidade)
+                    preco_medio = ((preco_medio_tmp * quantidade_media_tmp) + (
+                        preco * quantidade) + taxa_corretagem) / (quantidade_media_tmp + quantidade)
                     quantidade_media = quantidade_media_tmp + quantidade
-                    self.carteira.loc[index] = [data_operacao, acao, preco, preco_medio,quantidade, quantidade_media, operacao, resultado_auferido_temp, self.lucro_prejuizo]
+                    self.carteira.loc[index] = [data_operacao, acao, preco, preco_medio, quantidade,
+                                                quantidade_media, operacao,preco_medio*quantidade, resultado_auferido_temp, self.lucro_prejuizo]
                 else:
                     preco_medio = preco_medio_tmp
                     quantidade_media = quantidade_media_tmp - quantidade
-                    resultado_auferido = (preco - preco_medio_tmp) * quantidade - taxa_corretagem
+                    resultado_auferido = (
+                        preco - preco_medio_tmp) * quantidade - taxa_corretagem
                     self.lucro_prejuizo += resultado_auferido
                     imposto = self.calcula_imposto(resultado_auferido)
-                    self.carteira.loc[index] = [data_operacao, acao,preco, preco_medio,quantidade, quantidade_media, operacao, resultado_auferido, self.lucro_prejuizo - imposto]
+                    self.carteira.loc[index] = [data_operacao, acao, preco, preco_medio, quantidade,
+                                                quantidade_media, operacao,preco_medio*quantidade, resultado_auferido, self.lucro_prejuizo - imposto]
--- main2.py
+++ main2.py
@@ -1,16 +1,34 @@
 from calculadora import Calculadora
 import pandas as pd
-import numpy as np
+import plotly.express as px
+
 
 
 calculadora = Calculadora('stocks-dataset.csv')
 calculadora.calcular_carteira()
 
 
-output1 = pd.DataFrame(columns=['Mes', 'Valor total de Compra/R$', 'Valor total de Venda/R$',
-                       'Rendimento Bruto/R$', 'Imposto Devido/R$', 'Rendimento Liquido/R$'])
+output1 = pd.DataFrame(columns=['Mes', 'Compra/R$', 'Venda/R$', 'Rendimento Bruto/R$', 'Imposto/R$', 'Rendimento Liquido/R$'])
+
 
 output1['Mes'] = calculadora.carteira['Data'].dt.month_name().unique()
 
-#print soma de quantidade de compra por mes
-print(calculadora.carteira.loc[calculadora.carteira['Oper'] == 'Compra'].groupby(calculadora.carteira['Data'].dt.month_name())['Quantidade'].sum())
+for index, row in output1.iterrows():
+        mes = row['Mes']
+        compras_df = calculadora.carteira[(calculadora.carteira['Oper'] == 'Compra') & (calculadora.carteira['Data'].dt.month_name() == mes)]
+        valor_compra_total = compras_df['Valor Executado'].sum()
+        vendas_df = calculadora.carteira[(calculadora.carteira['Oper'] == 'Venda') & (calculadora.carteira['Data'].dt.month_name() == mes)]
+        valor_vendas_total = vendas_df['Valor Executado'].sum()
+        rendimento_absoluto_bruto = calculadora.carteira[calculadora.carteira['Data'].dt.month_name() == mes].loc[:,'Resultado Auferido'].sum() 
+        imposto_devido = calculadora.calcula_imposto(rendimento_absoluto_bruto) if rendimento_absoluto_bruto > 0 else 0
+        rendimento_liquido = rendimento_absoluto_bruto - imposto_devido
+        output1.loc[index] = [mes, valor_compra_total, valor_vendas_total, rendimento_absoluto_bruto, imposto_devido, rendimento_liquido ] 
+         
+
+output1 = round(output1, 2)
+output1.to_csv('informações_consolidadas.csv', index=False)
+
+
+fig = px.line(output1, x='Mes', y=['Compra/R$', 'Venda/R$', 'Rendimento Bruto/R$', 'Imposto/R$', 'Rendimento Liquido/R$'])
+fig.show()
+
